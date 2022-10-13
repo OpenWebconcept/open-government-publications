8f5ab9a045cfe52d44516e8674277a6af2ebc4ad
Sander de Kroon <sanderdekroon@live.nl>
2022-10-13 14:36:34 +0200

    Release 2.0.6

--- open-government-publications.php
+++ open-government-publications.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Open Government Publications
  * Description: Import and publish Government Publications through the REST API.
- * Version: 2.0.5
+ * Version: 2.0.6
  * Author: Súdwest-Fryslân
  * Author URI: https://sudwestfryslan.nl/
  * Requires at least: 5.1
